9ea48a4bfef66e9c4a1f5d6083167192c13de76f
TheDamrek <kermadabdenour73@gmail.com>
2026-08-14 22:09:28 +0100

    basic functions done

---
+++ Calculator.js
@@ -0,0 +1,37 @@
+const ButtonContainer = document.querySelector('.ButtonContainer')
+const OperationsBox = document.querySelector('.OperationsBox')
+
+const add = function(numbers) {
+      let total = 0
+   for(let i = 0; i < numbers.length; i++){
+   total += numbers[i]  
+}
+return total 
+};
+
+
+const substract = function(numbers) {
+       let total = numbers[0]
+   for(let i = 1; i < numbers.length; i++){
+   total -= numbers[i]  
+}
+return total
+};
+
+
+const multiply = function(numbers) {
+   let total = numbers[0]
+   for(let i = 1; i < numbers.length; i++){
+   total *= numbers[i]  
+}
+return total
+}
+
+const divide = function(numbers) {
+          let total = numbers[0]
+   for(let i = 1; i < numbers.length; i++){
+   total /= numbers[i]  
+}
+return total
+
+}
